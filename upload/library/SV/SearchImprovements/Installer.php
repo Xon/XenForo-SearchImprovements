26c64cee3d46ad64119161ed6851a59091009551
Xon <tazipper1@hotmail.com>
2016-11-24 01:13:12 +0800

    tidy up installer

--- upload/library/SV/SearchImprovements/Installer.php
+++ upload/library/SV/SearchImprovements/Installer.php
@@ -13,12 +13,15 @@ class SV_SearchImprovements_Installer
 
         SV_Utils_Install::addColumn('xf_user_option', 'sv_default_search_order', "varchar(50) NOT NULL default ''");
 
-        $db = XenForo_Application::getDb();
-        // make sure the model is loaded before accessing the static properties
-        XenForo_Model::create("XenForo_Model_User");
-        $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int) values
-            (?, 0, 'general', 'sv_searchOptions', 'allow', '0')
-        ", array(XenForo_Model_User::$defaultRegisteredGroupId));
+        if ($version < 1010300)
+        {
+            $db = XenForo_Application::getDb();
+            // make sure the model is loaded before accessing the static properties
+            XenForo_Model::create("XenForo_Model_User");
+            $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int) values
+                (?, 0, 'general', 'sv_searchOptions', 'allow', '0')
+            ", array(XenForo_Model_User::$defaultRegisteredGroupId));
+        }
 
         SV_Utils_Install::removeOldAddons(array('SV_ElasticSearchInfo' => array()));
     }
